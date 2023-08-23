53affb6e275fba1131d8d7cc1e754a43a4fb71c2
Brittany Brimley <brittanysapp@gmail.com>
2023-08-23 10:58:40 -0600

    added content, questions and functions in index.js to create README.md file

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const { writeFile } = require("fs/promises");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What would you like to add to your Installation section?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What would you like to add to your Usage section?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Which license would you like to use?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What would you like to add to your contribution section?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What would you like to add to your tests section?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What is your GitHub username?" && "What is your email address?"
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
+const readMe = () => {
+    inquirer.prompt(questions).then((data) => {
+        writeFile("README.md", layout(data))
+    });
+};
 
 // Function call to initialize app
-init();
+readMe();
