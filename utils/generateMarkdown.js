9c1c434c15e38de0d3c2b62a99327afe906a1902
Brittany Brimley <brittanysapp@gmail.com>
2023-09-22 17:26:42 -0600

    deleted ToDo comment

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,7 +28,7 @@ function renderLicenseLink(data) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, returns an empty string
 function renderLicenseSection(data) {
   if(data.license === "None") {
