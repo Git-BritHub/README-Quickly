1b7d73b6a590abcf78b804ba01ee5e3cba11382c
Brittany Brimley <brittanysapp@gmail.com>
2023-09-22 12:43:37 -0600

    edited and added detailed descriptions and instructions for this repos README.md file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(data) {
   if(data.license === "None") {
     return ""
   } else if(data.license === "MIT License") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-aqua.svg)](https://opensource.org/licenses/MIT)"
   } else if(data.license === "Apache License 2.0") {
     return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
   } else if(data.license === "BSD 3-Clause License") {
